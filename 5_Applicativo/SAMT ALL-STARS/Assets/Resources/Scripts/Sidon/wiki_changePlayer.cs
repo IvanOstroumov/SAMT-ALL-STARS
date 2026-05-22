2e65124fde620430cd32a4e1c3d7d8005775036a
Sidon08 <sidonsml08@gmail.com>
2026-05-22 12:33:18 +0200

    Mattina 22/05/26

--- 5_Applicativo/SAMT ALL-STARS/Assets/Resources/Scripts/Sidon/wiki_changePlayer.cs
+++ 5_Applicativo/SAMT ALL-STARS/Assets/Resources/Scripts/Sidon/wiki_changePlayer.cs
@@ -2,24 +2,31 @@ using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using Resources.Scripts;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class wiki_changePlayer : MonoBehaviour
 {
+    private CharacterManager characterManager;
+    private Character characterSelected;
     private String tag = "PlayerGroup";
+    public GameObject wins;
+    public GameObject hp;
+    public GameObject speed;
+    public GameObject story;
+    
     void Start()
     {
-
+        characterManager = GameManager.Instance.characterManager;
+        characterSelected = characterManager.getCharByName("yasser");
+        ChangeTexts();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void changePlayer(GameObject select) 
     {
+        characterSelected = characterManager.getCharByName(select.name);
         GameObject[] groups = GameObject.FindGameObjectsWithTag(tag);
         foreach (GameObject group in groups) 
         {
@@ -27,7 +34,6 @@ public class wiki_changePlayer : MonoBehaviour
          
             if (select.Equals(group))
             {
-                Debug.Log(group.name);
                 transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0f);
             }
             else 
@@ -35,5 +41,16 @@ public class wiki_changePlayer : MonoBehaviour
                 transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 1300f);
             }
         }
+        ChangeTexts();
+    }
+
+    private void ChangeTexts()
+    {
+
+        speed.GetComponent<Text>().text = characterSelected.Data.Speed.ToString(CultureInfo.CurrentCulture);
+        hp.GetComponent<Text>().text = characterSelected.Data.MaxHp.ToString();
+        story.GetComponent<Text>().text = characterSelected.Data.Description;
+        wins.GetComponent<Text>().text = characterSelected.Data.WinCount.ToString();
+
     }
 }
